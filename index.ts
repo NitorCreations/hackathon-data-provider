c1a6757a513565e6e681f152f30c325c2cdb4d47
Riku Laukkanen <riku.laukkanen@nitor.com>
2022-05-03 13:49:03 +0300

    add endpoints to return resources

--- index.ts
+++ index.ts
@@ -1,18 +1,35 @@
 import express from 'express'
-import { user } from "./src/factories/User";
+import { fileHandler } from "./src/fileHandler";
 const app = express()
 
 const PORT = 3000;
-
-const users = Array.from({ length: 10 }).map((_, i) => user({ name: "nimi" + i }))
 
 app.get('/',  (req, res) => {
     res.send('Hello World')
 })
 
 app.get('/users', async (req, res) => {
+    const users = await fileHandler("./data/users.json")
     res.header("Content-Type",'application/json');
-    res.send(JSON.stringify(users))
+    res.send(users)
+})
+
+app.get('/activities', async (req, res) => {
+    const activities = await fileHandler("./data/activities.json")
+    res.header("Content-Type",'application/json');
+    res.send(activities)
+})
+
+app.get('/fromFlights', async (req, res) => {
+    const fromFlights = await fileHandler("./data/fromFlights.json")
+    res.header("Content-Type",'application/json');
+    res.send(fromFlights)
+})
+
+app.get('/toFlights', async (req, res) => {
+    const toFlights = await fileHandler("./data/toFlights.json")
+    res.header("Content-Type",'application/json');
+    res.send(toFlights)
 })
 
 app.listen(PORT)
