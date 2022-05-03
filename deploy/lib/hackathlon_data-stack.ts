0523672422813c335b88e81d75710c680722f6c1
Mikko Tiihonen <mikko.tiihonen@nitor.com>
2022-05-03 16:15:06 +0300

    Add cdk deploy

---
+++ deploy/lib/hackathlon_data-stack.ts
@@ -0,0 +1,29 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+
+export class HackathlonDataStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "HackathlonVpc", {
+      maxAzs: 1
+    });
+
+    const cluster = new ecs.Cluster(this, "HackathlonCluster", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "HackathlonFargateService", {
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 1,
+      taskImageOptions: { image: ecs.ContainerImage.fromAsset('..') },
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true
+    });
+  }
+}
